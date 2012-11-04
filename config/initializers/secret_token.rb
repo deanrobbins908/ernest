b168a93129d8a36ac4f9f86b3efa1cc64650502b
Dean Robbins <dean.robbins@gmail.com>
2012-11-04 12:39:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ernest::Application.config.secret_token = 'c6ba50e9a6be1dd61833d6547a13b85a976c0bb0bd91207384fbd98db8a9c7986921043c8ba3da8c0e6853e5946f1adab65bd6a9a0b1409206257f89a93f1387'
